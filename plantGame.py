bd5cfe445b3ae76188c4b7cf028033f8f01038ff
Alvin Shek <ashek@andrew.cmu.edu>
2018-05-02 16:42:25 -0400

    Add files via upload
    unsolved: moving bugs, weeds showing up
    added: bomb, though wrong location

--- plantGame.py
+++ plantGame.py
@@ -35,7 +35,6 @@ def init(data):
     initColors(data)
     initCoreObjects(data)
     initPlants(data)
-    collectUsernames(data)
     setResistances(data)
     data.clicked = False
     data.paused = False
@@ -46,6 +45,8 @@ def init(data):
     data.loginBoxSelected = False
     data.monthSelected = False
     data.invalidFlag = False
+    data.bombSelected = False
+    data.bombPlantNeg = False
 
 def initStorages(data):
     data.controlVals = dict()
@@ -65,7 +66,6 @@ def initStorages(data):
     data.optimalFactors = [80,70,45]
     data.optimalSizes = [0]
     data.infectedPlants = []
-    data.allScores = set()
     data.infectProbs = dict()
     data.collidingPlants = set()
     data.plantLocs = dict()
@@ -73,28 +73,37 @@ def initStorages(data):
     data.bugFactorWeights = [.4,.2,.4] #adjustable, just hardcoded for now
     data.weedFactorWeights = [.2,.4,.4]
     data.monthBoxLoc = (8, data.cols//2)
+    bombRow,bombCol = data.bombSelectLoc
+    data.sliderLocs = [(data.buttonH,data.buttonX),(data.buttonH*2,data.buttonX),(data.buttonH*3,data.buttonX),(bombRow-data.bombRadius,bombCol)]
     
-def initGlobals(data):
+def initGlobals(data): #this information doesn't get wiped each level
+    data.allScores = set()
+    collectUsernames(data)
+    data.currentLevel = 1
     #location of bomb
+    row,col = data.bombSelectLoc
+    data.bombList = [(row,col,data.bombRadius,data.timerCalled,
+        "Inactive",data.currentLevel)]
     pass
 
 def initUI(data):
     data.rows = data.cols = 100
-    data.timerCalled = 1
+    data.timerCalled = 0
     data.baseGrowthRate = 0
+    data.bombRadius = 5
     data.bugSpawnProb = 0
     data.weedSpawnProb = 0
     data.month = "7"
     data.endSize = 0
     data.endTime = 50
     data.timeFactor = 3
-    data.weatherTime = 15
+    data.weatherTime = 7
     data.totalScore = 0 
-    data.growthTime = 5
-    data.spawnTime = 10
+    data.growthTime = 2
+    data.spawnTime = 5
     data.optGrowth = 0
     data.plantCount = 0
-    data.bombLoc = data.rows/4, data.cols*3/4
+    data.bombSelectLoc = (data.rows/4, data.cols*3/4)
     data.buttonH = data.rows//30
     data.buttonL = data.buttonH*5
     data.sliderL = data.buttonH*8
@@ -102,14 +111,13 @@ def initUI(data):
     data.buttonX = data.cols*3/4
     data.cellSize = data.width/data.rows
     data.slideR = data.buttonH #one cell
-    data.sliderLocs = [(data.buttonH,data.buttonX),(data.buttonH*2,data.buttonX),(data.buttonH*3,data.buttonX)]
     data.plantSize = 5
     data.textH = 15
     data.butLine = 2
     data.imageSide = data.cols//4
     data.runButLoc = data.rows*9/10,data.cols*7/10
     data.loginBoxLoc = data.rows/10, data.cols/2
-    data.weedSize = 2
+    data.weedSize = 4
     
 def initCoreObjects(data):
     data.coreObjects = dict()
@@ -179,7 +187,10 @@ def trackSliderBalls(data):
         cy = (row+data.sliderH/2)*data.cellSize
         #sliderBallLocs uses rows, cols for key, actual location for value
         data.sliderBallLocs[(row,col)]=(cx,cy)
-        data.controlVals[(row,col)]=[data.userValues[inc],data.valueTypes[inc]]
+        if inc <=2:
+            data.controlVals[(row,col)]=[data.userValues[inc],
+                    data.valueTypes[inc]]
+        
         
 def linReg(data):
     if data.simRunning: return
@@ -244,17 +255,23 @@ def selectPlants(event,data):
     #         data.sliderBallLocs[slider] = (cx,cy)
 
 def updateSlider(data,event):
+    inc = 0
     for slider in data.sliderBallLocs:
+        inc+=1
         cx,cy = data.sliderBallLocs[slider]
         if checkSliderBounds(data,cx,cy,event,slider):
             difference = (event.x-cx)//(data.sliderL/2)
             if difference >0:
                 difference*=1.75
-            data.controlVals[slider][0] += difference
+            if inc <=0:
+                data.controlVals[slider][0] += difference
+            else:
+                data.bombRadius += difference
             cx = event.x
             data.sliderBallLocs[slider] = (cx,cy)
 
 def updatePlants(data):
+    sumGrowth = 0
     data.allTimes.append(data.timerCalled//data.growthTime)
     for species in data.plantSpecies:
         #if len(data.plantImages[species]) == 1: continue
@@ -268,18 +285,20 @@ def updatePlants(data):
                         change = infectChange(data)
                         rate -= change
             if (row,col) in data.collidingPlants:
-                rate -= 5#idk about this
-            size += rate/data.timeFactor
+                rate -= 5 #idk about this
+            size += rate/data.timeFactor*data.currentLevel
             if size <= 0: size = 0
+            sumGrowth+= size
             data.plantImages[species][inc] =(row,col,size,rate)
             data.allPlantSizes[species][(row,col)].append(size)
             data.totalScore += (size)
             prevSize = data.optimalSizes[-1]
             data.optimalSizes.append(data.optGrowth+prevSize)
-            
+    if sumGrowth == 0: endGame(data)
+
 def infectChange(data):
     day = data.timerCalled//20
-    return math.e**(0.1*(day-10))
+    return math.e**(0.1*(day-15))
     
 def moveObjects(data,event):
     #these all have built-in checks for location
@@ -302,14 +321,27 @@ def clickGarden(event,data):
     if (x0<event.x<x1) and (y0<event.y<y1):
         return True
     
+def clickDeathSpot(event,data):
+    for inc in range(1,len(data.bombList)):
+        row,col,size,startTime,state,level = data.bombList[inc]
+        cx = col*data.cellSize
+        cy = row*data.cellSize
+        distance = math.sqrt((cx-event.x)**2+(cy-event.y)**2)
+        if distance <= size:
+            return True
+    return False
+    
 def createPlant(event,data):
-    row = event.y/data.cellSize
-    col = event.x/data.cellSize
-    data.plantImages[data.selectedPlant].append((row,col,data.plantSize,0))
-    data.allPlantSizes[data.selectedPlant][(row,col)]= [data.plantSize]
-    data.plantLocs[data.selectedPlant].add((row,col))
-    data.plantCount +=1
-    calcInfectedProb(data)
+    if clickDeathSpot(event,data):
+        data.bombPlantNeg = True
+    else:
+        row = event.y/data.cellSize
+        col = event.x/data.cellSize
+        data.plantImages[data.selectedPlant].append((row,col,data.plantSize,0))
+        data.allPlantSizes[data.selectedPlant][(row,col)]= [data.plantSize]
+        data.plantLocs[data.selectedPlant].add((row,col))
+        data.plantCount +=1
+        calcInfectedProb(data)
     
 def checkBounds(data,event):
     pass
@@ -442,11 +474,10 @@ def spawnWeed(data):
     distanceDistrib = norm(0,10)
     for (neighborRow,neighborCol) in data.allWeeds:
         distance = math.sqrt((row-neighborRow)**2+(col-neighborCol)**2)
-        neighborFactor = distanceDistrib.pdf(distance)*5
+        neighborFactor = distanceDistrib.pdf(distance)*10
         baseProb += neighborFactor*100 #convert to percentage
     if random.randint(0,100) <= baseProb:
         data.allWeeds.add((row,col))
-        print("yes")
         data.weedInfo[(row,col)] = data.weedSize
     
 def calcBugs(data):
@@ -615,6 +646,13 @@ def checkResults(data):
         pass
         #move on to next level
     
+def endGame(data):
+    data.simRunning = False
+    data.simEnd = True
+    calcEndSize(data)
+    updateBombs(data)
+    showGraph(data)
+    recordUsername(data)
     
 def spreadBugs(data):
     newSpreadPlants = []
@@ -663,6 +701,7 @@ def drawPlants(canvas,data):
     
 def drawWeeds(canvas,data):
     for weed in data.allWeeds:
+        print(data.allWeeds)
         (row,col) = weed
         size = data.weedInfo[(row,col)]
         cy = row*data.cellSize
@@ -746,10 +785,12 @@ def drawBugs(canvas,data):
 def drawTextAndBut(canvas,data):
     createButton(canvas,data,data.runButLoc,"Run Simulation")
     drawText(canvas,data,1,data.cols/4,"Test Your Knowledge of Plants")
-    drawText(canvas,data,3,data.cols/4,"by Choosing the Optimal Growth Parameters!")
+    drawText(canvas,data,3,data.cols/4,
+            "by Choosing the Optimal Growth Parameters!")
     temp, bright, moist = list(data.controlVals.values())
     drawText(canvas,data,1,data.cols*3/4,"Temperature: %d Brightness: %d Moisture: %d"%(temp[0],bright[0],moist[0]))
-    drawText(canvas,data,data.rows/2,data.cols*3/4,"Press 'd' to delete last plant")
+    drawText(canvas,data,data.rows/2,data.cols*3/4,
+            "Press 'd' to delete last plant")
     
     drawText(canvas,data,5,data.cols/4,
             "Hot Weather Probability:%.2f"%data.weatherProbs[0])
@@ -758,10 +799,14 @@ def drawTextAndBut(canvas,data):
     drawText(canvas,data,9,data.cols/4,
             "Rain Probability:%.2f"%data.weatherProbs[2])
     if data.noneEntered:
-        drawText(canvas,data,data.rows/2,data.cols/2,"Don't Leave It Blank!",fill="red")
+        drawText(canvas,data,data.rows/2,data.cols/2,
+            "Don't Leave It Blank!",fill="red")
     for plant in data.plantImages:
         row,col,size,rate = data.plantImages[plant][0]
         drawText(canvas,data,row-size*3/4,col,plant)
+    if data.bombPlantNeg:
+        drawText(canvas,data,data.rows/4-data.bombRadius-3, data.cols*3/4,
+            "Can't Add Plants to Bombed Areas!",fill="red")
     
 def drawCoreObjects(canvas,data):
     for object in data.coreObjects:
@@ -777,14 +822,53 @@ def drawCoreObjects(canvas,data):
                 drawCell(canvas,data,row,col,color)
 
 def drawBomb(canvas,data):
-    row,col = data.bombLocs
-    r = data.buttonL
-    x0 = col*data.cellSize
-    y0 = row*data.cellSize
-    canvas.create_oval(x0-r,y0-r,x0+r,y0+r,fill="black")
-    canvas.create_rectangle(x0,y0,x0+data.buttonL*data.cellSize,y0+data.textH,
-        fill="black")
+    for inc in range(len(data.bombList)):
+        row,col,size,startTime,state,level = data.bombList[inc]
+        x0 = col*data.cellSize
+        y0 = row*data.cellSize
+        if state == "Inactive":
+            canvas.create_oval(x0-size,y0-size,x0+size,y0+size,fill="black")
+            if inc != 0: #don't have the first table bomb explode
+                timer = int(data.timerCalled-startTime)
+                drawText(row,col,str(timer),fill="yellow")
+        elif state == "Explode":
+            canvas.create_oval(x0-size,y0-size,x0+size,y0+size,fill="orange")
+        elif state == "Dead":
+            canvas.create_oval(x0-size,y0-size,x0+size,y0+size,fill="grey")
         
+def clickBomb(event,data):
+    row,col,size,startTime,state,level = data.bombList[0]
+    x0 = (col-size)*data.cellSize
+    y0 = (row-size)*data.cellSize
+    x1 = x0+size*2*data.cellSize
+    y1 = y0+size*2*data.cellSize
+    if (x0<event.x<x1) and (y0<event.y<y1):
+        data.bombSelected = True
+        
+def createBomb(event,data):
+    if data.bombSelected:
+        row = event.y/data.cellSize
+        col = event.x/data.cellSize
+        data.bombList.append((row,col,data.bombRadius,data.timerCalled,"Inactive",data.currentLevel))
+    
+def updateBombs(data):
+    for inc in range(len(data.bombList)):
+        (row,col,size,startTime,state,level) = data.bombList[inc]
+        age = data.timerCalled-startTime
+        if inc == 0:
+            size = data.bombRadius
+            data.bombList[inc] = (row,col,size,startTime,state,level)
+            continue
+        if level < data.currentLevel:
+            #don't need to update old bombs
+            state = "Dead"
+        if 30<= age <= 50: #show explosion animation
+            state = "Explode"
+        elif age > 50:
+            state = "Dead"
+        if data.simEnd: state = "Dead"
+        data.bombList[inc] = (row,col,size,startTime,state,level)
+            
 
 def drawBoard(canvas,data):
     #draw basic background of the board, which can include pieces if locked in
@@ -911,6 +995,7 @@ def setupGardenMousePressed(event, data):
         data.boxFill = "grey"
         
 def simGardenMousePressed(event, data):
+    if data.simEnd or not data.simRunning: return
     clickRow = event.y/data.cellSize
     clickCol = event.x/data.cellSize
     for (row,col) in data.allWeeds:
@@ -918,7 +1003,10 @@ def simGardenMousePressed(event, data):
         if distance <= data.weedInfo[(row,col)]:
             data.allWeeds.remove((row,col))
             del data.weedInfo[(row,col)]
-
+    clickBomb(event,data)
+    if clickGarden(event,data):
+        createBomb(event,data)
+    data.selectedBomb = False
 
 def loginKeyPressed(event,data):
     data.noneEntered = False
@@ -946,15 +1034,19 @@ def setupGardenKeyPressed(event, data):
             calcWeatherProb(data)
     
 def simGardenKeyPressed(event, data):
+    if data.simEnd or not data.simRunning: return
     if event.char =="p": data.paused = not data.paused
     if event.char =="q":
-        data.mode = "setupGarden"
+        init(data)
+        initGlobals(data)
 
 def setupGardenTimerFired(data):
     pass
 
 def simGardenTimerFired(data):
-    if data.simEnd or not data.simRunning: checkResults(data)
+    if data.simEnd or not data.simRunning:
+        checkResults(data)
+        return
     data.timerCalled+=1
     moveRain(data)
     spazzBug(data)
@@ -971,11 +1063,7 @@ def simGardenTimerFired(data):
         callWeatherEvents(data)
         calcBugs(data)
     if data.timerCalled == data.endTime:
-        data.simRunning = False
-        data.simEnd = True
-        calcEndSize(data)
-        showGraph(data)
-        recordUsername(data)
+        endGame(data)
 
 def loginTimerFired(data):
     pass
@@ -985,7 +1073,7 @@ def setupGardenRedrawAll(canvas, data):
     drawSlider(canvas,data,data.sliderLocs[0])
     drawSlider(canvas,data,data.sliderLocs[1])
     drawSlider(canvas,data,data.sliderLocs[2])
-    drawSlider(canvas,data,data.sliderLocs[2])
+    drawSlider(canvas,data,data.sliderLocs[3])
     drawCoreObjects(canvas,data)
     drawPlants(canvas,data)
     #instructions 
@@ -995,14 +1083,19 @@ def setupGardenRedrawAll(canvas, data):
     drawText(canvas,data,boxRow,boxCol,str(data.month))
     drawText(canvas,data,boxRow-2.75,boxCol,"Current Month")
     
+    
 def simGardenRedrawAll(canvas,data):
     drawBoard(canvas,data)
     drawCoreObjects(canvas,data)
     drawPlants(canvas,data)
     drawRainDrops(canvas,data)
     drawBugs(canvas,data)
+    drawBomb(canvas,data)
     drawText(canvas,data,1,data.cols/2,"Bug Prob:%.2f"%data.bugSpawnProb)
-    drawText(canvas,data,2,data.cols/2,"Weed Prob:%.2f"%data.weedSpawnProb)
+    drawText(canvas,data,3,data.cols/2,"Weed Prob:%.2f"%data.weedSpawnProb)
+    if data.bombSelected:
+        drawText(canvas,data,3,data.cols/2,
+            "Bomb Selected! Click Out White Space to Negate",fill="red")
     if data.simEnd:
         drawLeaderBoard(canvas,data)
         
@@ -1072,6 +1165,7 @@ def run(width=300, height=300):
     data.timerDelay = 100 # milliseconds
     data.root = Tk()
     init(data)
+    initGlobals(data)
     # create the data.root and the canvas
     canvas = Canvas(data.root, width=data.width, height=data.height)
     canvas.pack()
