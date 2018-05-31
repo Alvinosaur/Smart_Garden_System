f1fcf49d2f04bcd513a7f00af748e4341612d5ce
Alvin Shek <ashek@andrew.cmu.edu>
2018-05-30 19:28:05 -0700

    Added 404 Error and plant detail page

--- garden_data/see_data/views.py
+++ garden_data/see_data/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
 from .models import Plants
 
 #request is the HTTP website url
 def index(request):
 	all_plants = Plants.objects.all()
 
-	#get_template assuems you've created a template folder in current app directory
-	template = loader.get_template('see_data/index.html')
-
 	#context is standard name for dict that holds info used by template
-	context = {
-		"all_plants":all_plants,
-	}
-	return HttpResponse(template.render(context,request))
+	context = {"all_plants":all_plants
+			}
+	return render(request,'see_data/index.html',context)
 
 
 def detail(request,plant_id):
-	return HttpResponse("<h2>Details for plant id: " + str(plant_id) + "</h2>")
+	try:
+		plant = Plants.objects.get(pk=plant_id)
+	except Plants.DoesNotExist:
+		raise Http404("Plant does not exist")
+	return render(request,'see_data/detail.html',{"plant":plant})
 
 
